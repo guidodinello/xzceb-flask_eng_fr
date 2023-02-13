6ad93b9b4f8a1304db132f115b07c0ce70c86250
Guido Dinello <guido.dinello@gmail.com>
2023-02-13 16:24:56 -0300

    tests modified

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,63 +1,19 @@
 import unittest
 from translator import english_to_french, french_to_english
-from typing import TypeVar
-
-TranslationFunction = TypeVar('TranslationFunction', english_to_french, french_to_english)
-AssertionMethod = TypeVar('AssertionMethod', unittest.TestCase.assertEqual, unittest.TestCase.assertNotEqual)
 
 class TestMachineTranslation(unittest.TestCase):
 
-    def _test_translate(self, 
-        assert_func : AssertionMethod,
-        trans_func : TranslationFunction, 
-        samples : "list[tuple[str, str]]"
-        ) : 
-        for (input, expected) in samples:
-            assert_func(trans_func(input), expected)
-
     def test_english_to_french(self):
-
-        self._test_translate( 
-            self.assertEqual,
-            english_to_french,
-        [
-            ("My name is Foo", "Mon nom est Foo"),
-            ("This is a test", "Il s'agit d'un test"),
-            ("Hello", "Bonjour")
-        ])
-
-        self._test_translate( 
-            self.assertNotEqual,
-            english_to_french,
-        [
-            ("My name is Foo", "My name is Foo"),
-            ("This is a test", "This is a test"),
-            ("Hello", "Hello")
-        ])
-
-        self.assertIsNone(english_to_french(None))
+        f = english_to_french
+        self.assertEqual(f("Hello"), "Bonjour")
+        self.assertNotEqual(f("Hello"), "Hello")
+        self.assertIsNone(f(None))
 
     def test_french_to_english(self):
-
-        self._test_translate( 
-            self.assertEqual,            
-            french_to_english,
-        [
-            ("Mon nom est Foo", "My name is Foo"),
-            ("Il s'agit d'un test", "This is a test"),
-            ("Bonjour", "Hello")
-        ])
-
-        self._test_translate( 
-            self.assertNotEqual,            
-            english_to_french,
-        [
-            ("Mon nom est Foo", "Mon nom est Foo"),
-            ("Il s'agit d'un test", "Il s'agit d'un test"),
-            ("Bonjour", "Bonjour")
-        ])
-
-        self.assertIsNone(french_to_english(None))
+        f = french_to_english
+        self.assertEqual(f("Bonjour"), "Hello")
+        self.assertNotEqual(f("Bonjour"), "Bonjour")
+        self.assertIsNone(f(None))
 
 if __name__ == '__main__':
     unittest.main()
